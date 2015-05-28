5f19a53df657a56b376f4a6c1acfb50c71278da2
Thura Hlaing <trhura@gmail.com>
2015-05-28 15:54:15 +0630

    first commit

---
+++ trainings.py
@@ -0,0 +1,129 @@
+python = {
+    "course_title": "Python for System Administration",
+    "course_image": "img/courses/python.jpg",
+    "course_description": """Everything you need to know about network
+    programming using Python, using the latest versions of Python
+    networking modules. Foundations of Python Network Programming
+    benefits both scripters and serious application developers who
+    want a feature-rich, yet simple language to address web and
+    network application coding challenges.""",
+
+
+    "trainer_name": "Thura Hlaing – The Bugslayer",
+    "trainer_image": "img/trainers/thura.jpg",
+    "trainer_description": """This course is designed for students who are new
+    to programming, and want to learn how to build Android apps. You
+    don’t need any programming experience to take this course. If
+    you’ve been using a smartphone to surf the web and chat with
+    friends, then you’re our perfect target student!""",
+
+
+    "course_syllabus": [
+        "Syllabus Item",
+        "Syllabus Item",
+        "Syllabus Item",
+        "Syllabus Item",
+        "Syllabus Item",
+    ],
+
+    "course_prerequisites": [
+        "You must be familiar with Linux and CLI to fully this training.",
+        "This course is designed for students who are new to programming.",
+        "This course is designed for students who are new to programming.",
+    ],
+
+
+    "course_duration": "8 Weeks",
+    "course_time" : "9:30 am - 11:30 am (Sat, Sun)",
+    "course_price" : "145,000",
+    "course_discount": "ten percent for STUDENTS, BYOD, EARLY BIRDS",
+
+    "course_next_class": "4th July, 2015"
+
+}
+
+android = {
+    "course_title": "Developing Android Applications",
+    "course_image": "img/courses/android.jpg",
+    "course_description": """Everything you need to know about network
+    programming using Python, using the latest versions of Python
+    networking modules. Foundations of Python Network Programming
+    benefits both scripters and serious application developers who
+    want a feature-rich, yet simple language to address web and
+    network application coding challenges.""",
+
+
+    "trainer_name": "Thura Hlaing – The Bugslayer",
+    "trainer_image": "img/trainers/thura.jpg",
+    "trainer_description": """This course is designed for students who are new
+    to programming, and want to learn how to build Android apps. You
+    don’t need any programming experience to take this course. If
+    you’ve been using a smartphone to surf the web and chat with
+    friends, then you’re our perfect target student!""",
+
+
+    "course_syllabus": [
+        "Syllabus Item",
+        "Syllabus Item",
+        "Syllabus Item",
+        "Syllabus Item",
+        "Syllabus Item",
+    ],
+
+    "course_prerequisites": [
+        "You must be familiar with Linux and CLI to fully this training.",
+        "This course is designed for students who are new to programming.",
+        "This course is designed for students who are new to programming.",
+    ],
+
+
+    "course_duration": "Two months",
+    "course_time" : "9:30 am - 11:30 am (Sat, Sun)",
+    "course_price" : "145,000",
+    "course_discount": "ten percent for STUDENTS, BYOD, EARLY BIRDS",
+
+    "course_next_class": "4th July, 2015"
+}
+
+laravel = {
+    "course_title": "Web Development with Laravel",
+    "course_image": "img/courses/laravel.jpg",
+    "course_description": """Everything you need to know about network
+    programming using Python, using the latest versions of Python
+    networking modules. Foundations of Python Network Programming
+    benefits both scripters and serious application developers who
+    want a feature-rich, yet simple language to address web and
+    network application coding challenges.""",
+
+
+    "trainer_name": "Thura Hlaing – The Bugslayer",
+    "trainer_image": "img/trainers/thura.jpg",
+    "trainer_description": """This course is designed for students who are new
+    to programming, and want to learn how to build Android apps. You
+    don’t need any programming experience to take this course. If
+    you’ve been using a smartphone to surf the web and chat with
+    friends, then you’re our perfect target student!""",
+
+
+    "course_syllabus": [
+        "Syllabus Item",
+        "Syllabus Item",
+        "Syllabus Item",
+        "Syllabus Item",
+        "Syllabus Item",
+    ],
+
+    "course_prerequisites": [
+        "You must be familiar with Linux and CLI to fully this training.",
+        "This course is designed for students who are new to programming.",
+        "This course is designed for students who are new to programming.",
+    ],
+
+
+    "course_duration": "Two months",
+    "course_time" : "9:30 am - 11:30 am (Sat, Sun)",
+    "course_price" : "145,000",
+    "course_discount": "ten percent for STUDENTS, BYOD, EARLY BIRDS",
+
+    "course_next_class": "4th July, 2015"
+}
