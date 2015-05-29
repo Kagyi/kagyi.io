01ef61b625f614d0f09520683519246b2ed9455a
Thura Hlaing <trhura@gmail.com>
2015-05-29 21:59:11 +0630

    update python course

--- trainings.py
+++ trainings.py
@@ -1,46 +1,47 @@
-
-python = {
+PY4ADMIN = {
     "course_title": "Python for System Administration",
     "course_image": "img/courses/python.jpg",
-    "course_description": """Everything you need to know about network
-    programming using Python, using the latest versions of Python
-    networking modules. Foundations of Python Network Programming
-    benefits both scripters and serious application developers who
-    want a feature-rich, yet simple language to address web and
-    network application coding challenges.""",
+    "course_description": """This course will introduce basic python programming and how to use it effectively to automate and manage Linux/UNIX systems. Although this course will focus primarily on system administration, anyone with a keen interest to learn python is welcomed to join. """,
 
-
-    "trainer_name": "Thura Hlaing - The Bugslayer",
+    "trainer_name": "Thura Hlaing - The Lonesome Pythonista",
     "trainer_image": "img/trainers/thura.jpg",
-    "trainer_description": """This course is designed for students who are new
-    to programming, and want to learn how to build Android apps. You
-    don’t need any programming experience to take this course. If
-    you’ve been using a smartphone to surf the web and chat with
-    friends, then you’re our perfect target student!""",
+    "trainer_description": """<p>Thura's first crush was cplusplus, which he met during the summer break in high school. In college, he flirted on and off with Java and lisp; but he was never serious with them. And once he laid his eyes on python, he knew that she's the one he is going to spend the rest of his life together.</p>
+    <p>Since then, Thura has been writing scripts with python,
+    <a href='https://gist.github.com/trhura/c2219325042382a20dee'>whenever</a>
+    <a href='https://github.com/trhura/python-myanmar'>he</a>
+    <a href='https://github.com/trhura/mm-crawling-examples'>has</a>
+    <a href='https://gist.github.com/trhura/7366740'>a</a>
+    <a href='https://github.com/trhura/nautilus-renamer'>chance</a>.
+    Also in his previous short-lived life as network software engineer at Frontiir, Thura wrote many cool sripts for network autmoation like
+    <a href='https://gist.github.com/trhura/5998584'>these</a>
+    <a href='https://gist.github.com/trhura/5981683'>ones</a>.</p>
 
+    <p>As one of the very few python developers in Myanmar, Thura is now devoted to and convey
+    """,
 
     "course_syllabus": [
-        "Syllabus Item",
-        "Syllabus Item",
-        "Syllabus Item",
-        "Syllabus Item",
-        "Syllabus Item",
+        "Installation & Introduction",
+        "Primitive Types",
+        "Conditionals & Control Flow",
+        "Collections & List Comprehension",
+        "Modules and Functoins",
+        "Classes and little doze of OOP",
+        "Text Processing",
+        "Networking Automation",
     ],
 
     "course_prerequisites": [
-        "You must be familiar with Linux and CLI to fully this training.",
-        "This course is designed for students who are new to programming.",
-        "This course is designed for students who are new to programming.",
+        "Familiarity with Linux/UNIX systems – Ubuntu will be used excusively in this course.",
+        "Comfortable working with the commandline",
+        "Basic understanding of networking and Internet",
     ],
 
-
     "course_duration": "8 Weeks",
-    "course_time" : "9:30 am - 11:30 am (Sat, Sun)",
-    "course_price" : "145,000",
-    "course_discount": "ten percent for STUDENTS, BYOD, EARLY BIRDS",
+    "course_time" : "5:00 pm - 7:30 pm (Sat, Sun)",
+    "course_price" : "145,000 Kyat",
+    "course_discount": "TEN percent available for STUDENTS, BYOD, EARLY BIRDS",
 
     "course_next_class": "4th July, 2015"
-
 }
 
 android = {
