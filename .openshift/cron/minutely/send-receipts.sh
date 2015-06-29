3047d37af0231c32a8169ffdfe01a430255a33ea
Thura Hlaing <trhura@gmail.com>
2015-06-29 22:32:19 +0630

    added cron

---
+++ .openshift/cron/minutely/send-receipts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
+  touch $OPENSHIFT_DATA_DIR/last_run
+fi
+if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +1) ]]; then #run every 2 mins
+  rm -f $OPENSHIFT_DATA_DIR/last_run
+  touch $OPENSHIFT_DATA_DIR/last_run
+  date > $OPENSHIFT_LOG_DIR/last_date_cron_ran
+fi
