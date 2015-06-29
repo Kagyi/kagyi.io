ca61e64c25f3de0af18849019dd29a09a17fdb01
Thura Hlaing <trhura@gmail.com>
2015-06-29 23:24:41 +0630

    removed pre populations

--- static/js/enroll.js
+++ static/js/enroll.js
@@ -1,11 +1,4 @@
 $(function(){
-    $('#trainer_email').val("thura@kagyi.io");
-    $('#student_email').val("thura@kagyi.io");
-    $('#student_name').val("Thura Hlaing");
-    $('#student_phone').val("09 3200 900 650");
-    $('#student_address').val("No(2), West horse Race course rD, tamwe");
-    $('#course_fees').val("145000");
-    $('#payment_received').prop('checked', true);
 
     $('#trainer_keyfile').removeClass('form-control');
     $('#enroll-form').submit(function() {
