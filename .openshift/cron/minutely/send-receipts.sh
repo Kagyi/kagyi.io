4f9e4c4897bc0c47405958e047f6a1645ca9fb41
Thura Hlaing <trhura@gmail.com>
2015-06-29 22:40:06 +0630

    changed cron log

--- .openshift/cron/minutely/send-receipts.sh
+++ .openshift/cron/minutely/send-receipts.sh
@@ -5,5 +5,5 @@ fi
 if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +1) ]]; then #run every 2 mins
   rm -f $OPENSHIFT_DATA_DIR/last_run
   touch $OPENSHIFT_DATA_DIR/last_run
-  date > $OPENSHIFT_LOG_DIR/last_date_cron_ran
+  date > $OPENSHIFT_PYTHON_LOG_DIR/last_date_cron_ran
 fi
