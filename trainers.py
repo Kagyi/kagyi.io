069a0318162ae04dccea36c9453655b052657b72
Thura Hlaing <trhura@gmail.com>
2015-06-29 23:44:09 +0630

    added trainers public keys

--- trainers.py
+++ trainers.py
@@ -5,5 +5,20 @@ z0ef5j4IlLc1yXeeARHvJLDtw1GwPahhIWMbHSrdOPAyGIhzB4f+LgWtR81jpmcf
 C5znY8mnuTXlMstf7XjvbaPx/ne969ArjJ9BFUaIcBor18eRf8Cu3tdVhLb+YEM8
 KPpob64RSWkHhnnrFQIDAQAB
 -----END PUBLIC KEY-----
-"""
+""",
+
+    "htoomyatag.webdev@gmail.com": """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+JRpzzIHQ3BbKTx0y0v+QkFep
+3ExWYyA7gQOPhJ4DgmwUCtRWXotUgrw/bOZ0/Cwdv+XMxur+wcZRo5meagMrKVAh
+M1pNPIMNYhnX3zanYcBzbW78G9k7ySCxsbJVOvgRwa3SmmTSTXTVvakBqNvYB/xm
+Hpmj3ZZn/iKpNmOAkwIDAQAB
+-----END PUBLIC KEY-----""",
+
+    "thu.yemyat@gmail.com": """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCzjD4KYNy9szFbpdRUlGscD0/W
+FZzt4gCz839cF8dxUCy1bdkH2GzvIZ0N+kzszdgGbLo0ASZnAtPgi+GagoxiJ6LI
+ahHOhZ3v+6jDx5t4sKzr/xOC7WlYCxx53ddqW4Hm+0sf0yP3511jWcMxKlmaNrb6
+FP25zEPGorPw+xObMQIDAQAB
+-----END PUBLIC KEY-----"""
+
 }
